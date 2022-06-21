67ada9a9e23909908411049a16524c62200ebcf2
sem1on <sgorlov21@gmail.com>
2022-06-21 14:28:08 +0300

    создание компонентов

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Work!</h1>
-    </div>
-  );
+
+const App = () => {
+    return (
+        <Box width='400px' sx={{ widht: { x1: '1488px'}}} m='auto'>
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/exercise/:id' element={<ExerciseDetail/>}/>
+            </Routes>
+            <Footer/>
+        </Box>
+    );
 }
 
 export default App;
