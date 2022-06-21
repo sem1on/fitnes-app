da6b6d11c8f512320fcd595aa82044d2fb246ccb
sem1on <sgorlov21@gmail.com>
2022-06-21 14:53:29 +0300

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Stack } from '@mui/material';
+
+import Logo from '../assets/images/Logo.png';
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <Stack direction="row" justifyContent="space-around" 
+            sx={{ gap: { sm: '122px', xs: '40px'}, mt: { sm: '32px', xs: '20px'}, justifyContent: 'none'}} px="20px"
+        >
+            <Link to='/'>
+                <img src={Logo} alt="logo" style={{ widht: '48px', hight: '48px', margin: '0px 20px'}} />
+            </Link>
+            <Stack
+                direction="row"
+                gap="40px"
+                fontSize="24px"
+                aliginYtems="flex-end"
+            >
+                <Link to='/' style={{ textDecoration: 'nonr', color: '#3A1212', borderBottom: '3px solid #FF2625'}}>Home</Link>
+                <a href='#exercises' style={{ textDecoration: 'nonr', color: '#3A1212'}}>Exercises</a>
+            </Stack>
+        </Stack>
     );
 };
 
